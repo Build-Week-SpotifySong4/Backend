35f59e74a310763b0a28ddfdc766f5ca0bae55ab
agohorel <3317203+agohorel@users.noreply.github.com>
2020-02-29 13:45:13 -0500

    add db config

---
+++ knexfile.js
@@ -0,0 +1,52 @@
+require("dotenv").config();
+
+module.exports = {
+  development: {
+    client: "pg",
+    connection: {
+      database: "spotify_development",
+      port: 5555,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD
+    },
+    migrations: {
+      directory: "./db/migrations"
+    },
+    seeds: {
+      directory: "./db/seeds/dev"
+    },
+    useNullAsDefault: true
+  },
+
+  testing: {
+    client: "pg",
+    connection: {
+      database: "spotify_development",
+      port: 5555,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD
+    },
+    migrations: {
+      directory: "./db/migrations"
+    },
+    seeds: {
+      directory: "./db/seeds/test"
+    }
+  },
+
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: "./db/migrations"
+    },
+    seeds: {
+      directory: "./db/seeds/production"
+    },
+    useNullAsDefault: true
+  }
+};
