fb3e4f19d2090d5dc9b4d90fe74ba54cbc0a089c
agohorel <3317203+agohorel@users.noreply.github.com>
2020-02-29 14:03:37 -0500

    fix test db name in knexfile

--- db/connections.js
+++ db/connections.js
@@ -2,5 +2,4 @@ const knex = require("knex");
 const knexfile = require("../knexfile.js");
 const env = process.env.DB_ENV || "development";
 
-
-module.exports = knex(knexfile[env])
+module.exports = knex(knexfile[env]);
