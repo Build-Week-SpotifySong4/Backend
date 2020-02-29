7aa6b8a433c2ae7ab5103c8d98fa0ebed5a1964f
agohorel <3317203+agohorel@users.noreply.github.com>
2020-02-29 14:37:05 -0500

    add seeds
    Co-authored-by: Chris Oakes <chris.oakes@outlook.com>

---
+++ db/seeds/production/001 - add users.js
@@ -0,0 +1,23 @@
+const bcrypt = require("bcryptjs");
+const dummyPW = bcrypt.hashSync("123abc", 10);
+
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex("users")
+    .truncate()
+    .then(function() {
+      // Inserts seed entries
+      return knex("users").insert([
+        { username: "Jordan", password: dummyPW },
+        { username: "Chris", password: dummyPW },
+        { username: "Alex", password: dummyPW },
+        { username: "Benjamin", password: dummyPW },
+        { username: "Brian", password: dummyPW },
+        { username: "Baisali", password: dummyPW },
+        { username: "Jan", password: dummyPW },
+        { username: "Keanu", password: dummyPW },
+        { username: "Seth", password: dummyPW },
+        { username: "Tanner", password: dummyPW }
+      ]);
+    });
+};
---
+++ db/seeds/test/002 - add songs.js
@@ -0,0 +1,19 @@
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex("songs")
+    .truncate()
+    .then(function() {
+      // Inserts seed entries
+      return knex("songs").insert([
+        { spotify_id: "1EiR6pce8KgrYGmPGU5xjY" },
+        { spotify_id: "1EiR6pce8KgrYGmPGU5xjY" },
+        { spotify_id: "2lwwrWVKdf3LR9lbbhnr6R" },
+        { spotify_id: "21Phj46KeUHOWyZW9A9b7P" },
+        { spotify_id: "60Pe9j2pCBa4Zp4ztf5nhd" },
+        { spotify_id: "7f1zjZG77S891Uv4O68yfk" },
+        { spotify_id: "2FvD20Z8aoWIePi7PoN8sG" },
+        { spotify_id: "4iiWcajF1fEUpwcUewc464" },
+        { spotify_id: "4q650OiSDQIwccxDFpuuBm" }
+      ]);
+    });
+};
