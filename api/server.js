6cfff03ce92b4b76c0c1668ca828350f3f9b37bd
agohorel <3317203+agohorel@users.noreply.github.com>
2020-02-29 17:35:02 -0500

    apply middlware
    Co-authored-by: Chris Oakes <chris.oakes@outlook.com>

--- api/server.js
+++ api/server.js
@@ -2,17 +2,21 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 
+const server = express();
+
+// routes
 const authRouter = require("../auth/auth-router.js");
 const songsRouter = require("../songs/songs-router.js");
-
-const server = express();
+// middleware
+const authenticate = require("../middleware/restricted.js");
+const validateCreds = require("../middleware/validateCreds.js");
 
 server.use(express.json());
 server.use(cors());
 server.use(helmet());
 
 // routers
-server.use("/api/auth", authRouter);
-server.use("/api/songs", songsRouter);
+server.use("/api/auth", validateCreds, authRouter);
+server.use("/api/songs", authenticate, songsRouter);
 
 module.exports = server;
